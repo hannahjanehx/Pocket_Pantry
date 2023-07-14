08252ee75bbb5e462e33018a9620c1dc55de4965
Hannah Hickling <hannahhickling@yahoo.co.uk>
2023-07-14 10:00:52 +0100

    add routes

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -36,7 +36,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
         <div className="max-w-7xl mx-auto p-6 lg:p-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
             <a
-                href=""
+                href={route('login')}
                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
             >
                 <StyledHeader className="text-xl font-semibold text-gray-900 dark:text-white">
@@ -48,7 +48,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
             </a>
 
             <a
-                href=""
+                href={route('register')}
                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
             >
                 <StyledHeader className="text-xl font-semibold text-gray-900 dark:text-white">
