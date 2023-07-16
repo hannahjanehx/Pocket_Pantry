959771cf94b034103791c2374a5ab8c6c59012a3
Hannah Hickling <hannahhickling@yahoo.co.uk>
2023-07-16 16:27:33 +0100

    styling of logo on header

--- resources/js/Layouts/LandingLayout.tsx
+++ resources/js/Layouts/LandingLayout.tsx
@@ -12,7 +12,7 @@ export default function Guest({ children }: PropsWithChildren) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <LandingApplicationLogo className="w-80" />
+                <LandingApplicationLogo className="w-80" header={false} />
                 {children}
             </div>
         </div>
--- resources/js/components/ApplicationLogo.tsx
+++ resources/js/components/ApplicationLogo.tsx
@@ -4,12 +4,22 @@ import logo from '/public/images/pocket_pantry.png';
 const StyledImg = styled.img`
     margin: auto;
     margin-top: 10%;
+`;
+
+const HeaderImg = styled.img`
+    height: inherit;
 `
 
-export default function ApplicationLogo({ className = '' }) {
+export default function ApplicationLogo({ className = '', header = false }) {
+
+    let displayedImg = <HeaderImg src={logo} />;
+    if(!header) {
+        displayedImg = <StyledImg src={logo} />;
+    }
+    console.log(header);
     return (
         <div className={className}>
-            <StyledImg src={logo} />
+            {displayedImg}
         </div> 
     );
 }
