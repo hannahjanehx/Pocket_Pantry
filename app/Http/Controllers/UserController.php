9262d56fec010b7d10b204ce086439e8c8c8292e
Hannah Hickling <hannahhickling@yahoo.co.uk>
2023-07-16 15:25:33 +0100

    add user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for a given user.
+     */
+    public function show(string $id): View
+    {
+        return view('user.profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+}
